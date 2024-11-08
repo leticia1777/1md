7843d4e4b7eefc193166a19b25d234efadff077c
leticia1777 <lima.pires.leticia@escola.pr.gov.br>
2024-11-08 08:51:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function resultado ()
 
 var numero 1 = 10
 var numero 2 = 15 
-var numero 1+ numero 2
+var resultado= numero 1+ numero 2
+document.getElementById ('resultado').innerHTML= resultado;
 }
-document. get Element By ( resultado). Inter HTML= resultado'
